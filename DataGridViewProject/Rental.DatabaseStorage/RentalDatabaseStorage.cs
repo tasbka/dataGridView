2fbd55a478feb91eca706feb129401aa229f1543
tasbka <hukakaya@gmail.com>
2025-12-09 14:33:05 +0300

    правки

--- DataGridViewProject/Rental.DatabaseStorage/RentalDatabaseStorage.cs
+++ DataGridViewProject/Rental.DatabaseStorage/RentalDatabaseStorage.cs
@@ -9,6 +9,9 @@ namespace Rental.DatabaseStorage
     /// </summary>
     public class RentalDatabaseStorage : IStorage
     {
+        /// <summary>
+        /// Получить все автомобили из базы данных
+        /// </summary>
         public async Task<List<CarModel>> GetAllCarsAsync()
         {
             using var database = new RentalDatabaseContext();
@@ -16,6 +19,9 @@ namespace Rental.DatabaseStorage
             return cars;
         }
 
+        /// <summary>
+        /// Добавить новый автомобиль в базу данных
+        /// </summary>
         public async Task AddCarAsync(CarModel car)
         {
             using var database = new RentalDatabaseContext();
@@ -23,6 +29,9 @@ namespace Rental.DatabaseStorage
             await database.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Обновить автомобиль в бд
+        /// </summary>
         public async Task UpdateCarAsync(CarModel car)
         {
             using var database = new RentalDatabaseContext();
@@ -30,6 +39,9 @@ namespace Rental.DatabaseStorage
             await database.SaveChangesAsync();
         }
 
+        /// <summary>
+        /// Удалить Автоммобиль по id 
+        /// </summary>
         public async Task DeleteCarAsync(Guid id)
         {
             using var database = new RentalDatabaseContext();
@@ -41,6 +53,9 @@ namespace Rental.DatabaseStorage
             }
         }
 
+        /// <summary>
+        /// Найти по  id
+        /// </summary>
         public async Task<CarModel?> GetCarByIdAsync(Guid id)
         {
             using var database = new RentalDatabaseContext();
